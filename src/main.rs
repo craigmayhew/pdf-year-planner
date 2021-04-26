74f2d22a4699a3c1b34bc0aab44ef4ac7ec96e87
Craig Mayhew <craig@mayhew.io>
2021-04-26 23:02:07 +0100

    PDF now A4, Portrait, with 5mm margins

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,12 @@ fn main() -> std::io::Result<()> {
     "##;
     let mut pdf_app = PdfApplication::new().expect("Failed to init PDF application");
     let mut pdfout = pdf_app.builder()
-        .orientation(Orientation::Landscape)
-        .margin(Size::Inches(2))
-        .title("Awesome Foo")
+        .orientation(Orientation::Portrait)
+        .page_size(PageSize::A4)
+        .margin(Size::Millimeters(5))
+        .title("2021 Year Planner")
         .build_from_html(&html)
         .expect("failed to build pdf");
-
-    pdfout.save("foo.pdf").expect("failed to save foo.pdf");
+    pdfout.save("2021.pdf").expect("failed to save foo.pdf");
     Ok(())
 }
