27f08e5e9c38179504706c58e4b1c869c440d8cd
Craig Mayhew <craig@mayhew.io>
2021-04-26 23:57:25 +0100

    Now generating a year page and a page for every day with working links

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 use wkhtmltopdf::*;
 
+fn generate_html_style() -> String {
+    r##"
+    <style>
+        table {page-break-after: always;}
+        table.year tr td {padding: 3px; background: #999;}
+        table.day tr td {padding: 3px; background: #999; height: 20cm; width: 15cm;}
+    </style>"##.to_owned()
+}
+
+fn generate_html_table_year() -> String {
+    let mut html: String = "".to_owned();
+    for month in 1..13 {
+        html += "<tr>";
+        for day in 1..32 {
+            let link = "<a href=\"#day_".to_owned() + &month.to_string() + "_" + &day.to_string() + "\">" + &day.to_string() + "</a>";
+            html += "<td>";
+            html += &link;
+            html += "</td>";
+        }
+        html += "</tr>";
+    }
+    
+    r##"<table class="year" name="year">"##.to_owned() + &html + "</table>"
+}
+
+fn generate_html_table_days() -> String {
+    let mut html: String = "".to_owned();
+    for month in 1..13 {
+        html += "<tr>";
+        for day in 1..32 {
+            let table = r##"<table class="day" name="day_"##.to_owned() + &month.to_string() + "_" + &day.to_string() + r##""><tr><td></td></tr></table>"##;
+            html += &table;
+        }
+    }
+    html
+}
+
 fn main() -> std::io::Result<()> {
-    let html = r##"
-        <html>
-            <head></head>
-            <body>
-                <h1>Rust can haz PDFs</h1>
-                <a href="#page3">Link Text</a>
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                <div name="page3">
-                    <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
-                </div>
-            </body>
-        </html>
-    "##;
+    let html = "<html><head>".to_owned() + &generate_html_style() + "</head><body>" + &generate_html_table_year() + &generate_html_table_days() + "</body></html>";
     let mut pdf_app = PdfApplication::new().expect("Failed to init PDF application");
     let mut pdfout = pdf_app.builder()
         .orientation(Orientation::Portrait)
