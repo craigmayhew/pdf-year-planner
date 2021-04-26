05042bc7f074c9487e122d84b006eb9337d3ac61
Craig Mayhew <craig@mayhew.io>
2021-04-26 23:01:37 +0100

    Added working example internal page link

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use wkhtmltopdf::*;
 
 fn main() -> std::io::Result<()> {
-    let html = r#"<html><body><div>foo</div></body></html>"#;
+    let html = r##"
+        <html>
+            <head></head>
+            <body>
+                <h1>Rust can haz PDFs</h1>
+                <a href="#page3">Link Text</a>
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                <div name="page3">
+                    <img src="https://www.rust-lang.org/logos/rust-logo-512x512.png">
+                </div>
+            </body>
+        </html>
+    "##;
     let mut pdf_app = PdfApplication::new().expect("Failed to init PDF application");
     let mut pdfout = pdf_app.builder()
         .orientation(Orientation::Landscape)
